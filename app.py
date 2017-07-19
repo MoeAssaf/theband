2d3a5c3dc3c1083020937090c5f4986d589a0f88
Moe Assaf <mohammadassaf160@gmail.com>
2017-07-19 17:47:52 +0300

    added heroku and some other sh*t

--- app.py
+++ app.py
@@ -3,9 +3,8 @@ from random import *
 app = Flask(__name__)
 @app.route('/')
 def hello():
-	myList = ["Guitar","Piano","Violin"]
-	display = True
-	return render_template("bar.html",display=display,list=myList)
+	return render_template("bar.html")
+
 
 if __name__ =="__main__":
 	app.run()
