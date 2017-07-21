9cee6c80375213bc36ba413a6b394998c88f5022
Moe Assaf <mohammadassaf160@gmail.com>
2017-07-21 12:25:03 +0300

    Updated form

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import *
 from random import *
 app = Flask(__name__)
-@app.route('/')
+@app.route('/', methods=["GET","POST"])
 def hello():
-	return render_template("bar.html")
+	if request.method == "GET":
+		return render_template("bar.html")
+	else:
+		form= request.form
+		name= form["Name"]
+		email= form["Email"]
+		message= form["Message"]
+		print name +"_"+ email +"_"+ message
+		return render_template("bar.html")
+
 
 
 if __name__ =="__main__":
